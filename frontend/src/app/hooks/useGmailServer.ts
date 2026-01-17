ea775ddd911344ca5062fa3d45f90ba2fa244440
shriram-svg <72285290+Shriram-Vasudevan@users.noreply.github.com>
2026-01-17 01:41:45 -0500

    Basic Frontned Impl

---
+++ frontend/src/app/hooks/useGmailServer.ts
@@ -0,0 +1,26 @@
+import * as React from "react"
+
+export const useGmailserver = () => {
+    const serverURL = process.env.NEXT_PUBLIC_SERVER_URL;
+
+    const [email, setEmail] = React.useState("");
+    const [subject, setSubject] = React.useState("");
+    const [message, setMessage] = React.useState("");
+    const [scheduledTime, setScheduledTime] = React.useState("");
+
+    const scheduleEmail = async () => {
+        return await fetch(`${serverURL}/schedule-email`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, subject, message, scheduledTime }),
+        });
+    }
+
+    return {
+        email, setEmail,
+        subject, setSubject,
+        message, setMessage,
+        scheduledTime, setScheduledTime,
+        scheduleEmail,
+    }
+}
